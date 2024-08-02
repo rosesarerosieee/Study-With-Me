6982389cee3efe6a8821ad4c7ab445be0bf97257
rosesarerosieee <perezrose0211@gmail.com>
2024-08-02 13:17:22 +0800

    Now the timer is running even the you are not in the tab

--- src/components/study-function.js
+++ src/components/study-function.js
@@ -3,24 +3,51 @@ import './study.css'
 
 const Study =() => {
 
+    {/*Time variables */}
     const [time, setTime] = useState(0);
     const [isRunning, setIsRunning] = useState(false);
-    const timeRef = useRef(null);
+    const intervalRef = useRef(null);
+    const startTimeRef = useRef(null);
 
-    const [task, setTaks] = useState([]);
-    const [taskInput, settaskInput] = useState('');
+    {/*Task variables S*/}
+    const [tasks, setTasks] = useState([]);
+    const [taskInput, setTaskInput] = useState('');
 
-    useEffect(() => {
+    const updateTime = () => {
         if(isRunning){
-            timeRef.current = setInterval(() => {
-                setTime(prevTime => prevTime + 10);
-            },10);
-        } else if (!isRunning && timeRef.current){
-            clearInterval(timeRef.current);
+            const now = Date.now();
+            const elapsedTime = now - startTimeRef.current;
+            setTime(elapsedTime);
+        }
+    }
+
+{/*Time function */}
+    useEffect(() => {
+
+        if(isRunning){
+            startTimeRef.current = Date.now() - time;
+            intervalRef.current = setInterval(updateTime, 10);
+        }else if (!isRunning && intervalRef.current){
+            clearInterval(intervalRef.current);
+        }
+         
+        return () => {
+            clearInterval(intervalRef.current);
+          
         }
 
-        return () => clearInterval(timeRef.current);
     },[isRunning]);
+
+    useEffect(() => {
+
+        if(tasks.length === 0){
+            setIsRunning(false);
+        }
+        else{
+            setIsRunning(true);
+        }
+
+    },[tasks]);
 
     const handleStartStop = () => {
         setIsRunning(!isRunning);
@@ -30,7 +57,6 @@ const Study =() => {
         setIsRunning(false);
         setTime(0);
     }
-
 
     const formatTime = (time) => {
         const milliseconds = time % 1000;
@@ -46,6 +72,31 @@ const Study =() => {
         return `${formattedHours}:${formattedMinutes}:${formattedSeconds}.${formattedMilliseconds}`;
       };
 
+    {/*Task Functions */}
+      const handleSubmit = (e) => {
+        e.preventDefault();
+        if(taskInput.trim() !== '')
+        {
+            setTasks([...tasks, {text:taskInput, completed: false}]);
+            setTaskInput('');
+        }
+      };
+      
+      const handleCompleted = (id) => {
+        const updatedTasks = tasks.map((task, index) => 
+        index === id ? {...task, completed: !task.completed} : task
+        );
+        setTasks(updatedTasks);
+      };
+
+      const handleDelete = (index) => {
+
+        const deletedTasks = tasks.filter((_, i) => i !== index);
+
+        setTasks(deletedTasks);
+      };
+
+
     return (
         <>
         <div className="container">
@@ -55,6 +106,30 @@ const Study =() => {
 
                 <button onClick={handleReset} disabled={isRunning}>Reset</button>
                 <h2>{formatTime(time)}</h2>
+
+                <form onSubmit={handleSubmit}>
+                    <input
+                        type="text"
+                        value={taskInput}
+                        onChange={(e) => setTaskInput(e.target.value)}
+                        placeholder="Enter task"
+                    />
+
+                    <button type="submit">Enter task</button>
+                </form>
+
+                <ul>
+                    {tasks.map((task, index) => (
+                        <li key={index} 
+                        className={task.completed ? 'done' : ''}
+                        onClick={() => handleCompleted(index)}
+                        >   
+                            
+                            {task.text}
+                            <button onClick={(e) => {e.stopPropagation(); handleDelete(index)}}>Delete</button>
+                        </li>
+                    ))}
+                </ul>
                 
             </div>
         </div>
