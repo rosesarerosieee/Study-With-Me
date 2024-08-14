d002eaf5c738c8ed8bf07434f19359db20400f1e
rosesarerosieee <perezrose0211@gmail.com>
2024-08-14 15:40:40 +0800

    Added 6 lofi musics

--- src/components/audiofile.js
+++ src/components/audiofile.js
@@ -1,12 +1,27 @@
 import lofi1 from '../assets/1lofi.mp3';
 import lofi2 from '../assets/2lofi.mp3';
 import lofi3 from '../assets/3lofi.mp3';
-
+import lofi4 from '../assets/4lofi.mp3';
+import lofi5 from '../assets/5lofi.mp3';
+import lofi6 from '../assets/6lofi.mp3';
+import lofi7 from '../assets/7lofi.mp3';
+import lofi8 from '../assets/8lofi.mp3';
+import lofi9 from '../assets/9lofi.mp3';
+import lofi10 from '../assets/10lofi.mp3';
 
 export const audioFile = [
 
     {src: lofi1, name: 'For The Last Time', artist: 'Hoko, Lucie Cravero'},
     {src: lofi2, name: 'Astral Scape', artist: 'Prithvi'},
-    {src: lofi3, name: 'Shogun Street', artist:'Prithvi'}
+    {src: lofi3, name: 'Shogun Street', artist:'Prithvi'},
+    {src: lofi4, name: 'Enter the Coffee Shop', artist: 'Lofi Waiter'},
+    {src: lofi5, name:'Kingdom in Blue', artist:'Kupla'},
+    {src: lofi6, name: 'Child Afternoon', artist:'Lofi Waiter'},
+    {src: lofi7, name:'Shibuya', artist:'Lofi Waiter'},
+    {src: lofi8, name:'Girl with A Cappucinno', artist: 'Lofi Waiter'},
+    {src: lofi9, name: 'Midnight Snak', artist: 'Purple Cat'},
+    {src: lofi10, name: 'Missing You', artist: 'Purple Cat'},
+    
+
 
 ] ;
